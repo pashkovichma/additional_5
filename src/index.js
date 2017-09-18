ae2963f53d837462b8756ae9a6c38b6b313d7d54
pashkovichma <pashkovichma@gmail.com>
2017-09-19 01:16:06 +0300

    additional_5/feature:logic implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	var i = 0;
+	var flag = 0;
+	var res = true;
+	while(i < str.length-2){
+	  	for (var j = 0; j < bracketsConfig.length; j++){
+  			if (str[i] == bracketsConfig[j][0]&&str[i+1] == bracketsConfig[j][1]){
+	  			str=str.slice(0,i)+str.slice(-str.length+i+2);
+	  			flag=1;	
+  			}
+  		}
+  		i++;
+  		if(flag){
+  			i=0;
+  			flag=0;
+  		}
+  	}
+
+  	for(var k=0; k<bracketsConfig.length;k++){
+  		if (str[0] == bracketsConfig[k][0]&&str[1] == bracketsConfig[k][1]){
+	  		str=[];
+	  	}
+  	}
+	if (str.length){
+  	var res = false;
+  	}
+  	return(res);
+ }
